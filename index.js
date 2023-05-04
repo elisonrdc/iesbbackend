e789503ed493cf9a2c1b80d4466f8c2503b0b1ab
Elison Ribeiro <elisonribeirosccp1910@gmail.com>
2023-05-04 16:01:53 -0300

    Retornando registros no banco

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-    response.send('Hello World!')
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'elison',
+    host: 'localhost',
+    database: 'postgres',
+    password: '123@mudar',
+    port: 5432,
+})
+
+app.get('/', async (request, response) => {
+    await response.send('Hello World!')
+})
+
+app.get('/usuarios', async (request, response) => {
+    await pool.query('SELECT * FROM usuario', (error, results) => {
+        if (error) { throw error }
+        response.status(200).json(results.rows)
+    })
 })
 
 app.listen(port, () => {
